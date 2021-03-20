b79f84001b4fbf3bda77bda948d08d04312ccbaa
rescue88 <finik848848@gmail.com>
2021-03-20 22:47:41 +0200

    added new elements due to developing the whole app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,23 @@
 import React, { useState } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import AppAdmin from './components/AppAdmin';
-import AppUser from './components/AppUser';
+import AppAdmin from './components/AppAdmin/AppAdmin';
+import AppUser from './components/AppUser/AppUser';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 
 const App: React.FC = () => {
-  const [isAuth, setIsAuth] = useState<boolean>(false);
+  const [isAuth, setIsAuth] = useState<boolean>(true);
   return (
     <main className="main-container">
       <Header />
       <div className="content">
         <Switch>
-          <Route exact path="/admin" render={() => <AppAdmin isAuth={isAuth} />} />
+          <Route path="/admin" render={() => <AppAdmin isAuth={isAuth} />} />
           <Route path="/app" render={() => <AppUser />} />
           <Redirect to="/app" />
         </Switch>
       </div>
+      <Footer />
     </main>
   );
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core';
+
 import './index.css';
 import App from './App';
+import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
