8f187bec8ed47765af9d056e9734c15958ae06c0
rescue88 <finik848848@gmail.com>
2021-03-22 19:49:56 +0200

    Footer nav changes

--- client/src/components/Footer/FooterNav/FooterNav.tsx
+++ client/src/components/Footer/FooterNav/FooterNav.tsx
@@ -7,10 +7,8 @@ const FooterNav: FC = () => {
     return (
         <nav className={st.footer__menu}>
             <ul>
-                <li className={st.menu__item}><NavLink to="/app">Головна</NavLink></li>
-                <li className={st.menu__item}><NavLink to="/app/cart">Корзина</NavLink></li>
-                <li className={st.menu__item}><NavLink to="/app/delivery">Доставка</NavLink></li>
-                <li className={st.menu__item}><NavLink to="/app/guarantee">Гарантія</NavLink></li>
+                <li className={st.menu__item}><NavLink to="/app/use">Умови використання сайту</NavLink></li>
+                <li className={st.menu__item}><NavLink to="/app/sponsor">Спонсори</NavLink></li>
                 <li className={st.menu__item}><NavLink to="/app/about">Про нас</NavLink></li>
                 <li className={st.menu__item}><NavLink to="/app/contacts">Контакти</NavLink></li>
             </ul>
