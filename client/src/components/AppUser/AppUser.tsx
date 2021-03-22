19d254a50fd4f1363f12f12513a963f04b3e3052
rescue88 <finik848848@gmail.com>
2021-03-22 19:50:51 +0200

    continue developing a whole page template

--- client/src/components/AppUser/AppUser.tsx
+++ client/src/components/AppUser/AppUser.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Navbar from './Navbar/Navbar';
+import { Route } from 'react-router-dom';
+
+import Home from './Home/Home';
 
 const AppUser: React.FC = () => {
     return (
         <>
             <Navbar />
+            <section className="page-container">
+                <Route exact path="/app" render={() => <Home />} />
+            </section>
         </>
     )
 }
--- client/src/components/AppUser/Navbar/Navbar.tsx
+++ client/src/components/AppUser/Navbar/Navbar.tsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import { FC } from 'react';
 
-const Navbar: React.FC = () => {
+import st from './Navbar.module.css';
+import NavbarItem from './NavbarItem/NavbarItem';
+
+const Navbar: FC = () => {
     return (
-        <div>Navbar Page</div>
+        <nav className={st.nav}>
+            <div className={st.nav__block}>
+                <div className={st.header}>
+                    Головні сторінки
+                </div>
+                <hr/>
+                <div className={st.menu}>
+                    <NavbarItem pageLink="" pageName="Стартова сторінка" pageLinkIsExact={true} />
+                    <NavbarItem pageLink="products" pageName="Товари" />
+                    <NavbarItem pageLink="cart" pageName="Корзина" />
+                    <NavbarItem pageLink="discount" pageName="Знижки" />
+                </div>
+            </div>
+            <div className={st.nav__block}>
+                <div className={st.header}>
+                    Категорії товарів
+                </div>
+                <hr/>
+                <div className={st.menu}>
+                    <NavbarItem pageLink="kitchen-prod" pageName="Для кухні" />
+                    <NavbarItem pageLink="home-prod" pageName="Для дому" />
+                    <NavbarItem pageLink="climate-prod" pageName="Кліматична техніка" />
+                    <NavbarItem pageLink="accessories-prod" pageName="Аксесуари для гаджетів" />
+                    <NavbarItem pageLink="hygiene-prod" pageName="Особиста гігієна" />
+                </div>
+            </div>
+            <div className={st.nav__block}>
+                <div className={st.header}>
+                    Інформація
+                </div>
+                <hr/>
+                <div className={st.menu}>
+                    <NavbarItem pageLink="delivery" pageName="Доставка та оплата" />
+                    <NavbarItem pageLink="guarantee" pageName="Гарантія" />
+                    <NavbarItem pageLink="covid" pageName="Відповідь COVID-19" />
+                </div>
+            </div>
+        </nav>
     )
 }
 
