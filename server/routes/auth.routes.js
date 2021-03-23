b1dedab4ffcb01d88b02438cfde02bac37beb883
rescue88 <finik848848@gmail.com>
2021-03-23 15:49:49 +0200

    folder to store all logic with api routes

---
+++ server/routes/auth.routes.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+// import authCtrl from './../controllers/auth.controller.js';
+
+const router = Router();
+
+router.route('/auth/signin')
+    .post(authCtrl.signin);
+
+router.route('/auth/signout')
+    .get(authCtrl.signout);
+
+export default router;
---
+++ server/routes/user.routes.js
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+// import userCtrl from './../controllers/user.controller.js';
+// import authCtrl from './../controllers/auth.controller.js';
+
+const router = Router();
+
+router.route('/api/users')
+    .get(userCtrl.list)
+    .post(userCtrl.create);
+
+router.param('userId', userCtrl.userByID);
+
+router.route('/api/users/:userId')
+    .get(authCtrl.requireSignin, userCtrl.read)
+    .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
+    .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove);
+
+export default router;
