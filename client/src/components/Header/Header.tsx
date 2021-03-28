a54ff1aae604cd54df41f1e15fe3c3f6977f03b5
rescue88 <finik848848@gmail.com>
2021-03-28 17:10:08 +0300

    refactoring Header Component

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,57 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
-import { Select, MenuItem, InputLabel } from '@material-ui/core';
-
+import { FC } from 'react';
 import st from './Header.module.css';
-import logo from './../../assets/icons/logo.svg';
-import ThemeToggle from './ThemeToggle/ThemeToggle';
+import HeaderNav from './HeaderNav/HeaderNav';
+import Logo from './Logo/Logo';
+import SearchForm from './SearchForm/SearchForm';
 
-const Header: React.FC = () => {
-    const [themeToggled, setThemeToggled] = useState<boolean>(true);
-
-    const handleToggleClick = () => {
-        setThemeToggled(prev => !prev);
-    }
-
-    useEffect(() => {
-        console.log(themeToggled)
-    }, [themeToggled]);
-
+const Header: FC = () => {
     return (
         <header className={st.header}>
-            <NavLink className={st.header__logoBlock} to="/app">
-                <div className={st.img}>
-                    <img src={logo} alt=""/>
-                </div>
-                <div className={st.name}>
-                    OZ Shop
-                </div>
-            </NavLink>
-            <div className={st.themeSwitch}>
-                <ThemeToggle themeToggled={themeToggled} onToggle={handleToggleClick} />
-            </div>
-            <div className={st.searchBlock}>
-                <div className={st.searchBlock__category}>
-                <InputLabel id="demo-controlled-open-select-label">Age</InputLabel>
-                <Select
-                    labelId="demo-controlled-open-select-label"
-                    id="demo-controlled-open-select"
-                    defaultValue=""
-                    // open={open}
-                    // onClose={handleClose}
-                    // onOpen={handleOpen}
-                    // value={age}
-                    // onChange={handleChange}
-                >
-                    <MenuItem value="">
-                        <em>None</em>
-                    </MenuItem>
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-                </div>
-            </div>
+            <Logo />
+            <SearchForm />
+            <HeaderNav />
         </header>
     );
 }
