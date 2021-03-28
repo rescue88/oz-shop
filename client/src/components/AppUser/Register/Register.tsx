8ee2fae946a22de1b5112d4213bf41c1cecb0c12
rescue88 <finik848848@gmail.com>
2021-03-28 22:49:57 +0300

    template for register Component

---
+++ client/src/components/AppUser/Register/Register.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import st from './Register.module.css';
+
+const Register: FC = () => {
+    return (
+        <div className={st.registerBlock}>
+            <h1>Register page</h1>
+        </div>
+    );
+}
+
+export default Register;
---
+++ client/src/components/AppUser/Register/RegisterForm/RegisterForm.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import st from './../Register.module.css';
+
+const RegisterForm: FC = () => {
+    return (
+        <div>
+            <h1>RegisterForm</h1>
+        </div>
+    );
+}
+
+export default RegisterForm;
