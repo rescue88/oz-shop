12c4848fa830f805d9e93ae22eaf6018a08e49e8
rescue88 <finik848848@gmail.com>
2021-03-28 22:51:14 +0300

    same styled classes for all the common Components

---
+++ client/src/components/common/useStyles.ts
@@ -0,0 +1,28 @@
+import { makeStyles } from '@material-ui/core';
+
+// common styles for inputs/buttons
+export const useStyles = makeStyles({
+    textField: {
+        padding: '.3rem 0',
+        background: 'var(--def-back)',
+        fontSize: '1.6rem'
+    },
+    autoInput: {
+      fontSize: '1.6rem',
+      WebkitBoxShadow: '0 0 0 1000px var(--def-back) inset',
+      color: 'var(--secondary-color)'
+    },
+    helperText: {
+        background: 'var(--def-back)',
+        fontSize: '1.2rem'
+    },
+    submitButton: {
+        borderRadius: 0,
+        fontSize: '1.5rem',
+        background: 'linear-gradient(var(--linear-primary), var(--linear-secondary))',
+        transition: 'all 1s',
+        '&:hover': {
+            background: 'var(--secondary-color)',
+        }
+    }
+});
