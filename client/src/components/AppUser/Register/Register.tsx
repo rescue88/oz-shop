ce9fec78c2f7d3733c2f364d48a9866ced408f38
rescue88 <finik848848@gmail.com>
2021-03-29 20:09:04 +0300

    Register page Component whole logic

--- client/src/components/AppUser/Register/Register.tsx
+++ client/src/components/AppUser/Register/Register.tsx
@@ -1,10 +1,19 @@
 import { FC } from 'react';
+import { NavLink } from 'react-router-dom';
 import st from './Register.module.css';
+import RegisterForm from './RegisterForm/RegisterForm';
 
 const Register: FC = () => {
     return (
         <div className={st.registerBlock}>
-            <h1>Register page</h1>
+            <div className={st.registerHeader}>
+                Створення користувача
+            </div>
+            <hr/>
+            <RegisterForm />
+            <div className={st.gotoLogin}>
+                Уже є акаунт? Перейдіть на сторінку <NavLink className={st.link} to="/app/login">авторизації</NavLink>!
+            </div>
         </div>
     );
 }
--- client/src/components/AppUser/Register/RegisterForm/RegisterForm.tsx
+++ client/src/components/AppUser/Register/RegisterForm/RegisterForm.tsx
@@ -1,11 +1,143 @@
-import { FC } from 'react';
+import { Field, Form, Formik } from 'formik';
+import { FC, useState } from 'react';
+import validator from 'validator';
+import MyPasswordField from '../../../common/MyPasswordField';
+import MySubmitButton from '../../../common/MySubmitButton';
+import MyTextField from '../../../common/MyTextField';
 import st from './../Register.module.css';
 
+const ValidateLogin = (value: string): string => {
+    let error: string = '';
+    if(!value) {
+        error = "Логін обов'язковий";
+    } else if(!validator.isAlphanumeric(value)) {
+        error = "Лише літери та цифри";
+    } else if(!validator.isLength(value, {min: 4, max: 12})) {
+        error = "Довжина логіна від 4 до 12 символів";
+    }
+    return error;
+}
+
+const ValidatePersonalName = (value: string): string => {
+    let error: string = '';
+    if(!value) {
+        error = "Ім'я обов'язкове";
+    } else if(!/^[А-ЯЁа-яё]{4,20}$/.test(value)) {
+        error = "Некоректно введне ім'я";
+    }
+    return error;
+}
+const ValidateEmail = (value: string): string => {
+    let error: string = '';
+    if(!value) {
+        error = "Пошта обов'язкова";
+    } else if(!validator.isEmail(value)) {
+        error = "Некоректно введна пошта";
+    }
+    return error;
+}
+
+const ValidatePhone = (value: string): string => {
+    let error: string = '';
+    if(!value) {
+        error = "Телефон обов'язковий";
+    } else if(!validator.isMobilePhone(value, 'uk-UA')) {
+        error = "+[код країни][номер] або лише [номер]";
+    }
+    return error;
+}
+
+const ValidatePassword = (value: string): string => {
+    let error: string = '';
+    if(!value) {
+        error = "Пароль обов'язковий";
+    } else if(!/^[a-zA-Z0-9_]{7,20}$/.test(value)) {
+        error = "Некоректно введений пароль";
+    }
+    return error;
+}
+
+const ValidateRepeatPassword = (password: string, repeatPassword: string): string => {
+    let error: string = '';
+    if(password !== repeatPassword) {
+        error = "Паролі не співпадають!";
+    }
+    return error;
+}
+
 const RegisterForm: FC = () => {
+    const [showPass, setShowPass] = useState<boolean>(false);
+
+    const handleClickShowPassword = () => {
+        setShowPass(!showPass);
+    };
+    
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
+
     return (
-        <div>
-            <h1>RegisterForm</h1>
-        </div>
+        <Formik
+            initialValues={{
+                login: '',
+                personalName: '',
+                email: '',
+                phone: '',
+                password: '',
+                repeatPassword: ''
+            }}
+            onSubmit={(data, {setSubmitting}) => {
+                setSubmitting(true);
+                setTimeout(() => {
+                    console.log('submit', data);
+                    setSubmitting(false);
+                }, 3000);
+            }}
+        >
+            {
+                ({values, isSubmitting}) => (
+                    <Form>
+                        <div className={st.inputField}>
+                            <Field width={250} validate={ValidateLogin} name="login" placeholder="Введіть логін" type="text" as={MyTextField} />
+                        </div>
+                        <div className={st.inputField}>
+                            <Field width={250} validate={ValidatePersonalName} name="personalName" placeholder="Введіть ім'я" type="text" as={MyTextField} />
+                        </div>
+                        <div className={st.inputField}>
+                            <Field width={250} validate={ValidateEmail} name="email" placeholder="Введіть пошту" type="email" as={MyTextField} />
+                        </div>
+                        <div className={st.inputField}>
+                            <Field width={250} validate={ValidatePhone} name="phone" placeholder="Введіть телефон" type="text" as={MyTextField} />
+                        </div>
+                        <div className={st.inputField}>
+                            <Field 
+                                width={203}
+                                validate={ValidatePassword}
+                                name="password" 
+                                placeholder="Введіть пароль"
+                                showPass={showPass} 
+                                handleClickShowPassword={handleClickShowPassword} 
+                                handleMouseDownPassword={handleMouseDownPassword}
+                                as={MyPasswordField} 
+                            />
+                        </div>
+                        <div className={st.inputField}>
+                            <Field 
+                                width={250} 
+                                validate={() => ValidateRepeatPassword(values.password, values.repeatPassword)} 
+                                name="repeatPassword" 
+                                placeholder="Підтвердіть пароль" 
+                                type="password" 
+                                as={MyTextField} 
+                            />
+                        </div>
+                        <div className={st.submitBtn}>
+                            <MySubmitButton disabled={isSubmitting} text="Створити профіль" />
+                        </div>
+                    </Form>
+                )
+            }
+        </Formik>
     );
 }
 
