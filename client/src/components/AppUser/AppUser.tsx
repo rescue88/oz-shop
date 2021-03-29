7b2e9b4a49416e2c2a1c4ce13d7b36865e164214
rescue88 <finik848848@gmail.com>
2021-03-29 20:08:18 +0300

    drawing register page

--- client/src/components/AppUser/AppUser.tsx
+++ client/src/components/AppUser/AppUser.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Navbar from './Navbar/Navbar';
 import { Route } from 'react-router-dom';
-
 import Home from './Home/Home';
 import Login from './Login/Login';
+import Register from './Register/Register';
 
 const AppUser: React.FC = () => {
     return (
@@ -12,6 +12,7 @@ const AppUser: React.FC = () => {
             <section className="page-container">
                 <Route exact path="/app" render={() => <Home />} />
                 <Route exact path="/app/login" render={() => <Login />} />
+                <Route exact path="/app/register" render={() => <Register />} />
             </section>
         </>
     )
