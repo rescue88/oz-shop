d6f64e9b4580e8b46e9e7d30c13ec5c3155fd4bd
rescue88 <finik848848@gmail.com>
2021-03-29 17:59:16 +0300

    Validation logic in LoginForm

--- client/src/components/AppUser/Login/LoginForm/LoginForm.tsx
+++ client/src/components/AppUser/Login/LoginForm/LoginForm.tsx
@@ -5,6 +5,26 @@ import MySubmitButton from "../../../common/MySubmitButton";
 import MyTextField from "../../../common/MyTextField";
 import st from './../Login.module.css';
 
+const ValidateLogin = (value: string): string => {
+    let error: string = '';
+    if(!value) {
+        error = "Логін обов'язковий";
+    } else if(!/^[a-z0-9_]{3,15}$/.test(value)) {
+        error = "Некоректно введний логін";
+    }
+    return error;
+}
+
+const ValidatePassword = (value: string): string => {
+    let error: string = '';
+    if(!value) {
+        error = "Пароль обов'язковий";
+    } else if(!/^[a-zA-Z0-9_]{3,20}$/.test(value)) {
+        error = "Некоректно введений пароль";
+    }
+    return error;
+}
+
 const LoginForm: FC = () => {
     const [showPass, setShowPass] = useState<boolean>(false);
 
@@ -18,56 +38,43 @@ const LoginForm: FC = () => {
 
     return (
         <Formik
-                initialValues={{
-                    login: '',
-                    password: '',
-                }}
-                onSubmit={(data, {setSubmitting}) => {
-                    setSubmitting(true);
+            initialValues={{
+                login: '',
+                password: '',
+            }}
+            onSubmit={(data, {setSubmitting}) => {
+                setSubmitting(true);
+                setTimeout(() => {
                     console.log('submit', data);
                     setSubmitting(false);
-                }}
-                validate={
-                    (values) => {
-                        const errors: Record<string, string> = {login: '', password: ''}
-
-                        if(!/^[a-z0-9_-]{3,16}$/.test(values.login)) {
-                            errors.login = "Некоректно введений логін";
-                        }
-
-                        if(!/^[a-z0-9_-]{3,16}$/.test(values.password)) {
-                            errors.password = 'Некоректно введений пароль'
-                        }
-
-                        return errors;
-                    }
-                }
-            >
-                {
-                    ({values, errors, isSubmitting}) => (
-                        <Form>
-                            <div className={st.inputField}>
-                                <Field width={250} name="login" placeholder="Введіть логін" type="input" as={MyTextField} />
-                            </div>
-                            <div className={st.inputField}>
-                                <Field 
-                                    width={205}
-                                    name="password" 
-                                    placeholder="Введіть пароль"
-                                    showPass={showPass} 
-                                    handleClickShowPassword={handleClickShowPassword} 
-                                    handleMouseDownPassword={handleMouseDownPassword}
-                                    as={MyPasswordField} 
-                                />
-                            </div>
-                            <div className={st.submitBtn}>
-                                <MySubmitButton disabled={isSubmitting} text="Увійти" />
-                            </div>
-                            <pre>{JSON.stringify(values, null, 2)}</pre>
-                        </Form>
-                    )
-                }
-            </Formik>
+                }, 3000);
+            }}
+        >
+            {
+                ({isSubmitting}) => (
+                    <Form>
+                        <div className={st.inputField}>
+                            <Field width={250} validate={ValidateLogin} name="login" placeholder="Введіть логін" type="input" as={MyTextField} />
+                        </div>
+                        <div className={st.inputField}>
+                            <Field 
+                                width={205}
+                                validate={ValidatePassword}
+                                name="password" 
+                                placeholder="Введіть пароль"
+                                showPass={showPass} 
+                                handleClickShowPassword={handleClickShowPassword} 
+                                handleMouseDownPassword={handleMouseDownPassword}
+                                as={MyPasswordField} 
+                            />
+                        </div>
+                        <div className={st.submitBtn}>
+                            <MySubmitButton disabled={isSubmitting} text="Увійти" />
+                        </div>
+                    </Form>
+                )
+            }
+        </Formik>
     )
 }
 
