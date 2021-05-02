24cbfe9ac6e6f353c1774ea4acc2fab241fc79ad
rescue88 <finik848848@gmail.com>
2021-05-02 17:30:45 +0300

    testing useSelector to break login/register routes if user is authorized

--- client/src/components/AppUser/AppUser.tsx
+++ client/src/components/AppUser/AppUser.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
-import Navbar from './Navbar/Navbar';
+import { FC } from 'react';
 import { Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import Navbar from './Navbar/Navbar';
 import Home from './Home/Home';
 import Login from './auth/Login/Login';
 import Register from './auth/Register/Register';
+import { StateType } from '../../types/stateTypes';
 
-const AppUser: React.FC = () => {
+const AppUser: FC = () => {
+    const isAuth: boolean = useSelector((state: StateType) => state.auth.isAuth);
+
     return (
         <>
             <Navbar />
             <section className="page-container">
                 <Route exact path="/app" render={() => <Home />} />
-                <Route exact path="/app/login" render={() => <Login />} />
-                <Route exact path="/app/register" render={() => <Register />} />
+                {
+                    !isAuth && (
+                        <>
+                            <Route exact path="/app/login" render={() => <Login />} />
+                            <Route exact path="/app/register" render={() => <Register />} />
+                        </>
+                    )
+                }
             </section>
         </>
     )
