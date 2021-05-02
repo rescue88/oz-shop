fd61eec389d20567abb5441bc365cb4bc37d9445
rescue88 <finik848848@gmail.com>
2021-05-02 21:52:16 +0300

    added snackbarReducer

--- client/src/redux/reducers/rootReducer.ts
+++ client/src/redux/reducers/rootReducer.ts
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import { authReducer } from './authReducer';
 import { userReducer } from './userReducer';
+import { snackbarReducer } from './snackbarReducer';
 
 export const rootReducer = combineReducers({
     auth: authReducer,
-    user: userReducer
+    user: userReducer,
+    snackbar: snackbarReducer
 });
