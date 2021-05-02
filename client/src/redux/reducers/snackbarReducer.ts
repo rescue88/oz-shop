b6ad72f5ee24afe2f66246f64ee99b63a242e182
rescue88 <finik848848@gmail.com>
2021-05-02 21:49:54 +0300

    state-control when to show a snackbar

---
+++ client/src/redux/reducers/snackbarReducer.ts
@@ -0,0 +1,35 @@
+import { SnackbarStateType, SnackbarType } from './../../types/stateTypes';
+
+/* ACTIONS */
+const SET_SNACKBAR = 'snackbarReducer/SET_SNACKBAR';
+
+/* INITIAL STATE */
+const snackbarState: SnackbarStateType = {
+    snackbarOpen: false,
+    snackbarType: 'success',
+    snackbarMessage: ''
+}
+
+/* ACTION CREATORS */
+export const setSnackbar = (snackbarOpen: boolean, snackbarType: keyof typeof SnackbarType, snackbarMessage: string) => ({
+    type: SET_SNACKBAR,
+    snackbarOpen,
+    snackbarType,
+    snackbarMessage
+});
+
+/* REDUCER */
+export const snackbarReducer = (state: SnackbarStateType = snackbarState, action: any) => {
+    switch(action.type) {
+        case SET_SNACKBAR:
+            const { snackbarOpen, snackbarType, snackbarMessage } = action;
+            return {
+                ...state,
+                snackbarOpen,
+                snackbarType,
+                snackbarMessage
+            }
+        default:
+            return state;
+    }
+}
