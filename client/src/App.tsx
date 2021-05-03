dc1229303b1c969d26224dc838a12c3684752205
rescue88 <finik848848@gmail.com>
2021-05-03 19:47:18 +0300

    fixed routing / reloading page is fine

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,44 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
+import { useSelector } from 'react-redux';
 import { Switch, Route, Redirect } from 'react-router-dom';
+
 import AppAdmin from './components/AppAdmin/AppAdmin';
 import AppUser from './components/AppUser/AppUser';
 import MySnackbar from './components/common/MySnackbar/MySnackbar';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import { useAuth } from './hooks/auth.hook';
+import { StateType } from './types/stateTypes';
 
 const App: FC = () => {
-  const [isAuth, setIsAuth] = useState<boolean>(true);
+  const { ready } = useAuth();
+
+  const isAuth = useSelector((state: StateType) => state.auth.isAuth);
+  const userPerm = useSelector((state: StateType) => state.auth.permissons);
+  // TODO:　make a preloader
   return (
-    <main className="main-container">
-      <MySnackbar />
-      <Header />
-      <div className="content">
-        <Switch>
-          <Route path="/admin" render={() => <AppAdmin isAuth={isAuth} />} />
-          <Route path="/app" render={() => <AppUser />} />
-          <Redirect to="/app" />
-        </Switch>
-      </div>
-      <Footer />
-    </main>
+    <>
+      {
+        ready ? (
+          <main className="main-container">
+            <MySnackbar />
+            <Header />
+            <div className="content">
+              {
+                <Switch>
+                  {
+                    userPerm === 'admin' && <Route path="/admin" render={() => <AppAdmin isAuth={isAuth} />} />
+                  }
+                  <Route path="/app" render={() => <AppUser isAuth={isAuth} />} />
+                  <Redirect to="/app" />
+                </Switch>
+              }
+            </div>
+            <Footer />
+          </main>
+        ) : (<div style={{width: '100%', height: '100%', backgroundColor: 'red'}}>not ready</div>)
+      }
+    </>
   );
 }
 
--- client/src/components/AppUser/AppUser.tsx
+++ client/src/components/AppUser/AppUser.tsx
@@ -1,29 +1,30 @@
 import { FC } from 'react';
-import { Route } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 import Navbar from './Navbar/Navbar';
 import Home from './Home/Home';
 import Login from './auth/Login/Login';
 import Register from './auth/Register/Register';
 import { StateType } from '../../types/stateTypes';
+import { AppType } from '../../types/common';
 
-const AppUser: FC = () => {
-    const isAuth: boolean = useSelector((state: StateType) => state.auth.isAuth);
-
+const AppUser: FC<AppType> = ({isAuth}) => {
     return (
         <>
             <Navbar />
             <section className="page-container">
-                <Route exact path="/app" render={() => <Home />} />
-                {
-                    !isAuth && (
-                        <>
-                            <Route exact path="/app/login" render={() => <Login />} />
-                            <Route exact path="/app/register" render={() => <Register />} />
-                        </>
-                    )
-                }
+                <Switch>
+                    <Route exact path="/app" render={() => <Home />} />
+                    {
+                        !isAuth && (
+                            <>
+                                <Route exact path="/app/login" render={() => <Login />} />
+                                <Route exact path="/app/register" render={() => <Register />} />
+                            </>
+                        )
+                    }
+                    <Redirect exact to="/app" />
+                </Switch>
             </section>
         </>
     )
