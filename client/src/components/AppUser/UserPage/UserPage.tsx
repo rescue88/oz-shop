4cf8136d8d94251529b760b09e072d0c48a3841b
rescue88 <finik848848@gmail.com>
2021-05-07 22:40:39 +0300

    retrieving users info

--- client/src/components/AppUser/UserPage/UserPage.tsx
+++ client/src/components/AppUser/UserPage/UserPage.tsx
@@ -1,12 +1,16 @@
 import { FC } from 'react';
+import { useSelector } from 'react-redux';
+import { StateType, UserStateType } from '../../../types/stateTypes';
 
 import UserAdds from './UserAdds/UserAdds';
 import UserInfo from './UserInfo/UserInfo';
 
 const UserPage: FC = () => {
+    const userData: UserStateType = useSelector((state: StateType) => state.user);
+
     return (
         <div className="userPage">
-            <UserInfo />
+            <UserInfo userData={userData} />
             <UserAdds />
         </div>
     )
