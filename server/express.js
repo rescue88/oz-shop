f0439a8c9cef6c58f6d354f4fd85fa02012b73f9
rescue88 <finik848848@gmail.com>
2021-05-09 14:22:31 +0300

    added cors middleware / formidable and fs packages to parse images and save in DB

--- server/express.js
+++ server/express.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 // default middlewares
 app.use(express.json({extended: true}));
+app.use(cors());
 
 // listen to a base url and call routers
 app.use('/api/auth', require('./routes/auth.routes'));
