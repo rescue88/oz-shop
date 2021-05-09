74feb15a8f4604afac5bbaeaddce01bdf4cf5901
rescue88 <finik848848@gmail.com>
2021-05-09 14:21:49 +0300

    ready and tested update user route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
 const User = require('./../models/User.model');
+const formidable = require('formidable');
+const fs = require('fs');
 
 const { deleteUserPrivateInfo, parseDateUkr, userById } = require('./helpers/helpers');
 const router = Router();
@@ -31,22 +33,40 @@ router.put(
     '/update/:id',
     userById,
     async (req, res) => {
-        try {
-            const user = req.profile;
-            const newFields = req.body;
-            console.log(newFields);
-            console.log(typeof newFields.photo);
-    
-            return res.status(200).json({
-                message: 'Дані успішно оновлено',
-                success: true
-            });
-        } catch(e) {
-            res.status(400).json({
-                message: `Щось не так з роутом /user/update/:id ${e.message}`,
-                success: false,
-            })
-        }
+        let form = new formidable.IncomingForm();
+        form.parse(req, async (err, fields, files) => {
+            if(err) {
+                return res.status(400).json({
+                    message: "Не вдалося додати фото",
+                    success: false
+                });
+            }
+
+            let user = req.profile;
+
+            if(files.avatar) {
+                user.avatar.data = fs.readFileSync(files.avatar.path);
+                user.avatar.contentType = files.avatar.type;
+            } else {
+                delete fields.avatar;
+            }
+
+            user = Object.assign(user, fields);
+
+            try {
+                await user.save();
+
+                return res.status(200).json({
+                    message: 'Дані профіля успішно оновлено',
+                    success: true
+                });
+            } catch(e) {
+                return res.status(400).json({
+                    message: e.message,
+                    success: false
+                });
+            }
+        });
     }
 )
 
