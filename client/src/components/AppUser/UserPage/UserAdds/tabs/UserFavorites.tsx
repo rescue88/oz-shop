d4ce6bb3f8834d2a664de336b668b1802c6e3708
unknown <@finik848848@gmail.com>
2021-05-13 00:42:59 +0300

    favorites array test

--- client/src/components/AppUser/UserPage/UserAdds/tabs/UserFavorites.tsx
+++ client/src/components/AppUser/UserPage/UserAdds/tabs/UserFavorites.tsx
@@ -1,9 +1,30 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { StateType } from '../../../../../types/stateTypes';
 
 const UserFavorites: FC = () => {
+    const dispatch = useDispatch();
+    const {favorites} = useSelector((state: StateType) => state.user);
+
+    const getFavoritesHandler = async () => {
+        
+    }
+
     return (
         <div className="userAdds__favorites">
-            Favorites
+            {
+                favorites.length ? (
+                    <>
+                        {
+                            favorites.map(item => (
+                                <div key={item} className="userAdds__favorites_item">{item}</div>
+                            ))
+                        }
+                    </>
+                ) : (
+                    <div>Вкладка з доданими товарами порожня</div>
+                )
+            }
         </div>
     )
 };
