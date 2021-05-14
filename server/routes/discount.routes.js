d22a511e58d47af686f154869cba856957c378f0
unknown <@finik848848@gmail.com>
2021-05-14 17:59:17 +0300

    new router file for discounts

---
+++ server/routes/discount.routes.js
@@ -0,0 +1,102 @@
+const { Router } = require('express');
+const formidable = require('formidable');
+const fs = require('fs');
+
+const Discount = require('./../models/Discount.model');
+
+const router = Router();
+
+// get all available discounts
+router.get(
+    '/',
+    async (req, res) => {
+        try {
+            const discounts = await Discount.find({});
+
+            if(!discounts.length) {
+                return res.status(400).json({
+                    message: "Ви не додали жодної знижки",
+                    success: false
+                });
+            }
+
+            return res.status(200).json({
+                message: "Наявні знижки успішно отримано",
+                success: true
+            });
+        } catch(e) {
+            return res.status(400).json({
+                message: `Не вдалося отримати знижки; ${e.message}`,
+                success: false
+            });
+        }
+    }
+);
+
+// create new discount
+router.post(
+    '/create',
+    async (req, res) => {
+        let form = new formidable.IncomingForm();
+        form.parse(req, async(err, fields, files) => {
+            if(err) {
+                return res.status(400).json({
+                    message: "Не вдалося створити новий продукт",
+                    success: false
+                });
+            }
+
+            // check if discount already exists
+            const candidate = await Discount.findOne({name: fields.name});
+            if(candidate) {
+                return res.status(400).json({
+                    message: "Знижка з такою назвою вже є",
+                    success: false
+                });
+            }
+
+            // if file recieved, save two necessary fields to store in db
+            if(files.image) {
+                fields.image = {};
+                fields.image.data = fs.readFileSync(files.image.path);
+                fields.image.contentType = files.image.type;
+            } else {
+                delete fields.image;
+            }
+
+            try {
+                const discount = new Discount({...fields});
+
+                await discount.save();
+
+                return res.status(200).json({
+                    message: "Знижку успішно додано",
+                    success: true
+                });
+            } catch(e) {
+                return res.status(400).json({
+                    message: `Не вдалося створити нову знижку; ${e.message}`,
+                    success: false
+                });
+            }
+        });
+    }
+);
+
+// update discount
+router.put(
+    '/update',
+    async(req, res) => {
+
+    }
+);
+
+// delete discount
+router.delete(
+    '/delete',
+    async(req, res) => {
+
+    }
+);
+
+module.exports = router;
