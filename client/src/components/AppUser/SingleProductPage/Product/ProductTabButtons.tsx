f7a75d49d2f7ef5722e679031c7153782f6559c0
unknown <@finik848848@gmail.com>
2021-05-16 13:35:17 +0300

    adding product to favorites

--- client/src/components/AppUser/SingleProductPage/Product/ProductTabButtons.tsx
+++ client/src/components/AppUser/SingleProductPage/Product/ProductTabButtons.tsx
@@ -1,12 +1,25 @@
 import { FC } from 'react';
+import { useSelector } from 'react-redux';
+import { StateType } from '../../../../types/stateTypes';
 
-const ProductTabButtons: FC = () => {
+type ProductTabButtonsType = {
+    productId: string;
+    isFetching: boolean;
+    addToFavorites: () => void;
+    addToCart?: () => void;
+}
+
+const ProductTabButtons: FC<ProductTabButtonsType> = ({productId, isFetching, addToFavorites}) => {
+    const {favorites} = useSelector((state: StateType) => state.user);
+
     return (
         <div className="product__content_buttons">
             <div>
                 <button 
                     className="wishlistBtn borderRadius" 
                     type="button"
+                    onClick={addToFavorites}
+                    disabled={isFetching || favorites.map(item => item._id).includes(productId)}
                 >
                     До заміток
                 </button>
