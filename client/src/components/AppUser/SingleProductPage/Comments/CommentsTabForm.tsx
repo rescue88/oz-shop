2f4a564ef755ab0dff0b26161574a52c33134ea1
unknown <@finik848848@gmail.com>
2021-05-17 16:57:22 +0300

    ready and usable form to add a comment

--- client/src/components/AppUser/SingleProductPage/Comments/CommentsTabForm.tsx
+++ client/src/components/AppUser/SingleProductPage/Comments/CommentsTabForm.tsx
@@ -1,24 +1,31 @@
 import { Field, Form, Formik } from 'formik';
 import { FC } from 'react';
 
+import { getStorageItem } from '../../../../assets/helpers/helpers';
 import { ValidateComment } from '../../../../assets/validators/validators';
 import MySimpleRadioBtn from '../../../common/Input/MySimpleRadioBtn';
 import MySimpleTextarea from '../../../common/Input/MySimpleTextarea';
 import MySubmitButton from '../../../common/MySubmitButton';
+import { CommentsTabType } from './CommentsTab';
 
-const CommentsTabForm: FC = () => {
+type CommentsTabFormType = {
+    isFetching: boolean;
+    addHandler: (productId: string, userId: string, text: string, positive: boolean) => void;
+} & CommentsTabType;
+
+const CommentsTabForm: FC<CommentsTabFormType> = ({isFetching, productId, addHandler}) => {
     return (
         <div className="commentsForm">
             <Formik
                 initialValues={{
                     'text': '',
-                    'positive': ''
+                    'positive': 'positive'
                 }}
                 onSubmit={async (data, {setSubmitting, resetForm}) => {
                     setSubmitting(true);
 
-                    console.log(data);
-                    
+                    const userId: string = getStorageItem()!.userId;
+                    await addHandler(userId, productId, data.text, Boolean(data.positive));
 
                     setSubmitting(false);
 
@@ -26,7 +33,7 @@ const CommentsTabForm: FC = () => {
                 }}
             >
                 {
-                    ({isSubmitting, values}) => (
+                    ({isSubmitting}) => (
                         <Form>
                             <div className="form__input">
                                 <Field 
@@ -41,10 +48,21 @@ const CommentsTabForm: FC = () => {
                             </div>
                             <div className="commentsForm__radio">
                                 <MySimpleRadioBtn 
+                                    type="radio"
+                                    id="positive"
                                     name="positive"
-                                    value="hi"
+                                    value="positive"
                                     label="Позитивний"
-                                />  
+                                    disabled={isFetching}
+                                />
+                                <MySimpleRadioBtn 
+                                    type="radio"
+                                    id="negative"
+                                    name="positive"
+                                    value=""
+                                    label="Негативний"
+                                    disabled={isFetching}
+                                />
                             </div>
                             <div className="form__submit commentsForm__submit">
                                 <MySubmitButton 
