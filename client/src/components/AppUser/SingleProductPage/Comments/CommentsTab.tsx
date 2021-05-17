afdf0b6651b3169bb98c5a9224d8fa67af414c8a
unknown <@finik848848@gmail.com>
2021-05-17 16:57:05 +0300

    new params and function templates / loading variable

--- client/src/components/AppUser/SingleProductPage/Comments/CommentsTab.tsx
+++ client/src/components/AppUser/SingleProductPage/Comments/CommentsTab.tsx
@@ -1,10 +1,46 @@
-import { FC } from 'react';
+import { FC, useState, useCallback, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { createComment } from '../../../../redux/thunks/commentReducer';
+
 import CommentsTabForm from './CommentsTabForm';
 
-const CommentsTab: FC = () => {
+export type CommentsTabType = {
+    productId: string;
+}
+
+const CommentsTab: FC<CommentsTabType> = ({productId}) => {
+    const [isFetching, setIsFetching] = useState<boolean>(false);
+    const dispatch = useDispatch();
+
+    const getCommentsHandler = useCallback(async () => {
+        setIsFetching(true);
+        setIsFetching(false);
+    }, [dispatch]);
+
+    const addCommentHandler = useCallback(async (productId: string, userId: string, text: string, positive: boolean) => {
+        setIsFetching(true);
+
+        await dispatch(createComment(productId, userId, text, positive));
+
+        setIsFetching(false);
+    }, [dispatch]);
+
+    const updateCommentHandler = useCallback(async () => {
+        setIsFetching(true);
+        setIsFetching(false);
+    }, [dispatch]);
+
+    useEffect(() => {
+
+    }, [addCommentHandler]);
+
     return (
         <div className="singleProduct__content_comments">
-            <CommentsTabForm />
+            <CommentsTabForm 
+                addHandler={addCommentHandler} 
+                isFetching={isFetching} 
+                productId={productId} 
+            />
         </div>
     );
 }
