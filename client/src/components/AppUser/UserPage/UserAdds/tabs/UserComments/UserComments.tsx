5bff121cc88bc73b5eda04c24bbc6d2dc812e311
unknown <@finik848848@gmail.com>
2021-05-19 18:12:03 +0300

    user comments in profile

---
+++ client/src/components/AppUser/UserPage/UserAdds/tabs/UserComments/UserComments.tsx
@@ -0,0 +1,66 @@
+import { FC, useState, useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { StateType } from '../../../../../../types/stateTypes';
+import UserCommentsItem from './UserCommentsItem';
+import { clearUserComments, deleteComment, getUserComments } from './../../../../../../redux/reducers/commentReducer';
+import { getStorageItem } from '../../../../../../assets/helpers/helpers';
+import CommentProductTabLoader from '../../../../../common/Loader/CommentProductTabLoader';
+
+const UserComments: FC = () => {
+    const [isFetching, setIsFetching] = useState<boolean>(false);
+    const userId: string = getStorageItem()!.userId;
+    const dispatch = useDispatch();
+    const comments = useSelector((state: StateType) => state.comment.user);
+
+    const getComments = useCallback(async () => {
+        setIsFetching(true);
+
+        await dispatch(getUserComments(userId));
+
+        setIsFetching(false);
+    }, [dispatch]);
+
+    const deleteCommentHandler = useCallback(async (productId: string) => {
+        setIsFetching(true);
+
+        await dispatch(deleteComment(userId, productId));
+
+        setIsFetching(false);
+    }, [dispatch]);
+
+    useEffect(() => {
+        getComments();
+
+        return () => {
+            dispatch(clearUserComments());
+        }
+    }, [deleteCommentHandler]);
+
+    return (
+        <div className="userAdds__comments">
+            <div className="userAdds__comments_items">
+                {
+                    isFetching ? (
+                        Array(5).fill(0).map((item, idx) => <CommentProductTabLoader key={idx} />)
+                    ) : (
+                        comments.length ? (
+                            comments.map(item => (
+                                <UserCommentsItem 
+                                    key={item._id}
+                                    comment={item}
+                                    isFetching={isFetching}
+                                    deleteComment={deleteCommentHandler}
+                                />   
+                            ))
+                        ) : (
+                            <div className="no-items">Коментарі ще не додано</div>
+                        )
+                    )
+                }                    
+            </div>
+        </div>
+    )
+};
+
+export default UserComments;
---
+++ client/src/components/AppUser/UserPage/UserAdds/tabs/UserComments/UserCommentsItem.tsx
@@ -0,0 +1,38 @@
+import { FC } from 'react';
+import { NavLink } from 'react-router-dom';
+import Tooltip from '@material-ui/core/Tooltip';
+
+import { convertBuffer } from '../../../../../../assets/helpers/helpers';
+import { CommentUserType } from '../../../../../../types/stateTypes';
+import defaultProductPic from './../../../../../../assets/images/defaultProduct.png';
+import DeleteIcon from '../../../../../common/Icons/DeleteIcon';
+
+type UserCommentsItemType = {
+    comment: CommentUserType;
+    isFetching: boolean;
+    deleteComment: (productId: string) => void;
+}
+
+const UserCommentsItem: FC<UserCommentsItemType> = ({isFetching, comment, deleteComment}) => {
+    return (
+        <div className={`commentItem ${comment.positive ? 'positive' : 'negative'}`}>
+            <NavLink to={`/app/products/${comment.product}`} className="commentItem__image">
+                <img src={comment.image.data ? convertBuffer(comment.image.data.data) : defaultProductPic} alt="" />
+            </NavLink>
+            <div className="commentItem__content">
+                <div className="commentItem__content_name">Коментар до продукту:&nbsp;<span>{comment.name}</span></div>
+                <div className="commentItem__content_date">{comment.created}</div>
+                <div className="commentItem__content_text">{comment.text}</div>
+            </div>
+            <div className="commentItem__settings">
+                <div className="deleteComment">
+                    <Tooltip title="Видалити коментар" arrow>
+                        <button type="button" onClick={() => deleteComment(comment.product)}><DeleteIcon /></button>
+                    </Tooltip>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default UserCommentsItem;
