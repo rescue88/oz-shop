1a10f767de3f0b1e96456f4a9decfdebd246c45e
unknown <@finik848848@gmail.com>
2021-05-20 21:38:03 +0300

    new home page components

---
+++ client/src/components/AppUser/Home/HomeDiscounsSlider.tsx
@@ -0,0 +1,70 @@
+import { FC } from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { NavLink } from 'react-router-dom';
+
+import { DiscountItemType } from '../../../types/stateTypes';
+import defaultSliderImg from './../../../assets/slider.jpg';
+import PrevButton from './Arrows/PrevButton';
+import NextButton from './Arrows/NextButton';
+import { convertBuffer } from '../../../assets/helpers/helpers';
+
+export type SliderButtonsType = {
+    onClick?: () => void;
+}
+
+type HomeDiscounsSliderType = {
+    discounts: Array<DiscountItemType>;
+}
+
+const sliderSettings = {
+    dots: false,
+    infinite: true,
+    draggable: false,
+    adaptiveHeight: true,
+    autoplay: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: <PrevButton />,
+    nextArrow: <NextButton />
+}
+
+const HomeDiscounsSlider: FC<HomeDiscounsSliderType> = ({discounts}) => {
+    return (
+        <>
+            {
+                discounts.length ? (
+                    <div className="slider">
+                        <Slider {...sliderSettings} >
+                            {
+                                discounts.map(item => (
+                                    <NavLink to="/app" className="slider__item">
+                                        <img src={item.image.data ? convertBuffer(item.image.data.data) : defaultSliderImg} alt="available discount" />
+                                    </NavLink>
+                                ))
+                            }
+                        </Slider>
+                    </div>
+                ) : (
+                    <div className="slider">
+                        <Slider {...sliderSettings} >
+                            <div className="slider__item">
+                                <img src={defaultSliderImg} alt="default discount block" />
+                            </div>
+                            <div className="slider__item">
+                                <img src={defaultSliderImg} alt="efault discount block" />
+                            </div>
+                            <div className="slider__item">
+                                <img src={defaultSliderImg} alt="efault discount block" />
+                            </div>
+                        </Slider>
+                    </div>
+                )
+            }
+        </>
+    )
+}
+
+export default HomeDiscounsSlider;
---
+++ client/src/components/AppUser/Home/HomeLatestProducts.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+const HomeLatestProducts: FC = () => {
+    return (
+        <div className="homePage__latest">latest products</div>
+    );
+}
+
+export default HomeLatestProducts;
