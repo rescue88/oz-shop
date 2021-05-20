49ae2cd5881e83d8ee9a90bf13102593aed57e4e
unknown <@finik848848@gmail.com>
2021-05-20 18:32:15 +0300

    ready and tested template for storing all cart info in redux

--- client/src/redux/reducers/cartReducer.ts
+++ client/src/redux/reducers/cartReducer.ts
@@ -4,6 +4,9 @@ import { CartProdutType, CartStateType } from "../../types/stateTypes";
 const SET_TOTAL_PRICE = 'cartReducer/SET_TOTAL_PRICE';
 const SET_TOTAL_COUNT = 'cartReducer/SET_TOTAL_COUNT';
 const ADD_PRODUCT_TO_CART = 'cartReducer/ADD_PRODUCT_TO_CART';
+const DECREASE_PRODUCT_AMOUNT = 'cartReducer/DECREASE_PRODUCT_AMOUNT';
+const CLEAR_CART = 'cartReducer/CLEAR_CART';
+const DELETE_PRODUCT_FROM_CART = 'cartReducer/DELETE_PRODUCT_FROM_CART';
 
 /* INITIAL STATE */
 const cartState: CartStateType = {
@@ -34,6 +37,26 @@ export const addProductToCart = (payload: CartProdutType) => {
     }
 }
 
+export const deacreaseProductAmount = (payload: string) => {
+    return {
+        type: DECREASE_PRODUCT_AMOUNT,
+        payload
+    }
+}
+
+export const clearCart = () => {
+    return {
+        type: CLEAR_CART
+    }
+}
+
+export const deleteProductFromCart = (payload: string) => {
+    return {
+        type: DELETE_PRODUCT_FROM_CART,
+        payload
+    }
+}
+
 /* THUNKS */
 
 /* REDUCER */
@@ -49,7 +72,7 @@ export const cartReducer = (state: CartStateType = cartState, action: any) => {
                 ...state,
                 totalCount: action.payload
             }
-        case ADD_PRODUCT_TO_CART:
+        case ADD_PRODUCT_TO_CART: {
             const newItems = {
                 ...state.items,
                 [action.payload._id]: !state.items[action.payload._id]
@@ -67,6 +90,42 @@ export const cartReducer = (state: CartStateType = cartState, action: any) => {
                 items: newItems,
                 totalCount,
                 totalPrice
+            }
+        }
+        case DECREASE_PRODUCT_AMOUNT: {
+            const newItems = {...state.items}
+            const deletedItem = newItems[action.payload].pop();
+
+            return {
+                ...state,
+                items: newItems,
+                totalCount: state.totalCount - 1,
+                totalPrice: state.totalPrice - deletedItem!.price
+            }
+        }
+        case DELETE_PRODUCT_FROM_CART: {
+            // make items copy
+            const newItems = {...state.items};
+            // extract and delete field
+            const deletedItem = newItems[action.payload];
+            delete newItems[action.payload];
+            // get amount and price of a deleted field
+            const deleteItemCount = deletedItem.length;
+            const deletedItemPrice = deletedItem.reduce((sum, next) => sum + next.price, 0);
+
+            return {
+                ...state,
+                items: newItems,
+                totalCount: state.totalCount - deleteItemCount,
+                totalPrice: state.totalPrice - deletedItemPrice 
+            }
+        }
+        case CLEAR_CART:
+            return {
+                ...state,
+                items: {},
+                totalCount: 0,
+                totalPrice: 0
             }
         default:
             return state;
