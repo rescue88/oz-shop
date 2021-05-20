148df02735d1122c92c9fc9602836ac1caa16af7
unknown <@finik848848@gmail.com>
2021-05-20 18:33:04 +0300

    using redux actions in a cart component

--- client/src/components/AppUser/CartPage/CartPage.tsx
+++ client/src/components/AppUser/CartPage/CartPage.tsx
@@ -1,40 +1,78 @@
 import { FC } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addProductToCart, clearCart, deacreaseProductAmount, deleteProductFromCart } from '../../../redux/reducers/cartReducer';
 
+import { StateType } from '../../../types/stateTypes';
 import CartRegularIcon from '../../common/Icons/CartRegularIcon';
 import DeleteIcon from '../../common/Icons/DeleteIcon';
 import NavIcon from '../Navbar/NavIcon/NavIcon';
 import CartPageItem from './CartPageItem';
 
 const CartPage: FC = () => {
+    const dispatch = useDispatch();
+    const {items: cartItems, totalCount, totalPrice} = useSelector((state: StateType) => state.cart);
+
+    const increaseProductAmountHandler = (productId: string) => {
+        dispatch(addProductToCart(cartItems[productId][0]));
+    }
+
+    const decreaseProductAmountHandler = (productId: string) => {
+        dispatch(deacreaseProductAmount(productId));
+    }
+
+    const clearCartHandler = () => {
+        dispatch(clearCart());
+    }
+
+    const deleteProductHandler = (productId: string) => {
+        dispatch(deleteProductFromCart(productId));
+    }
+
     return (
-        <div className="cart">
-            <div className="cart__head">
-                <div className="cart__head_header">
-                    <CartRegularIcon />
-                    Корзина
-                </div>
-                <button className="cart__head_clear high-opacity">
-                    <DeleteIcon />
-                    Очистити корзину
-                </button>
-            </div>
-            <div className="cart__items">
-                {
-                    Array(5).fill(0).map((item, idx) => <CartPageItem key={idx} />)
-                }
-            </div>
-            <div className="cart__sum space-betw-row">
-                <div className="cart__sum_amount">Загальна кількість товарів: <span>7</span></div>
-                <div className="cart__sum_money">Сума замовлення: <span className="price">₴900</span></div>
-            </div>
-            <div className="cart__buttons space-betw-row">
-                <button className="cart__buttons_back high-opacity centered-row">
-                    <NavIcon />
-                    До товарів
-                </button>
-                <button className="cart__buttons_pay opacity">Оплатити зараз</button>
-            </div>
-        </div>
+        <>
+            {
+                Object.keys(cartItems).length ? (
+                    <div className="cart">
+                        <div className="cart__head">
+                            <div className="cart__head_header">
+                                <CartRegularIcon />
+                                Корзина
+                            </div>
+                            <button className="cart__head_clear high-opacity" onClick={clearCartHandler}>
+                                <DeleteIcon />
+                                Очистити корзину
+                            </button>
+                        </div>
+                        <div className="cart__items">
+                            {
+                                Object.keys(cartItems).map(item => (
+                                    <CartPageItem
+                                        key={cartItems[item][0]._id}
+                                        productItems={cartItems[item]}
+                                        increaseAmount={increaseProductAmountHandler}
+                                        decreaseAmount={decreaseProductAmountHandler}
+                                        deleteProduct={deleteProductHandler}
+                                    />
+                                ))
+                            }
+                        </div>
+                        <div className="cart__sum space-betw-row">
+                            <div className="cart__sum_amount">Загальна кількість товарів: <span>{totalCount}</span></div>
+                            <div className="cart__sum_money">Сума замовлення: <span className="price">₴{totalPrice}</span></div>
+                        </div>
+                        <div className="cart__buttons space-betw-row">
+                            <button className="cart__buttons_back high-opacity centered-row">
+                                <NavIcon />
+                                До товарів
+                            </button>
+                            <button className="cart__buttons_pay opacity">Оплатити зараз</button>
+                        </div>
+                    </div>
+                ) : (
+                    <div>Корзина порожня</div>
+                )
+            }
+        </>
     );
 }
 
--- client/src/components/AppUser/CartPage/CartPageItem.tsx
+++ client/src/components/AppUser/CartPage/CartPageItem.tsx
@@ -1,30 +1,53 @@
 import { FC } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import { convertBuffer } from '../../../assets/helpers/helpers';
+import { CartProdutType } from '../../../types/stateTypes';
 import CloseIcon from '../../common/Icons/CloseIcon';
 import MinusIcon from '../../common/Icons/MinusIcon';
 import PlusIcon from '../../common/Icons/PlusIcon';
-
 import defaultProduct from './../../../assets/images/defaultProduct.png';
 
-const CartPageItem: FC = () => {
+type CartPageItemType = {
+    productItems: Array<CartProdutType>;
+    increaseAmount: (productId: string) => void;
+    decreaseAmount: (productId: string) => void;
+    deleteProduct: (productId: string) => void;
+}
+
+const CartPageItem: FC<CartPageItemType> = ({productItems, increaseAmount, decreaseAmount, deleteProduct}) => {
+    const singleProduct = productItems[0];
+
     return (
+
         <div className="cartItem">
-            <div className="cartItem__image">
-                <img src={defaultProduct} alt="product to buy" />
-            </div>
-            <div className="cartItem__name">Холодильник Samsung A5</div>
+            <NavLink to={`/app/products/${singleProduct._id}`} className="cartItem__image">
+                <img src={singleProduct.image.data ? convertBuffer(singleProduct.image.data.data) : defaultProduct} alt="product to buy" />
+            </NavLink>
+            <div className="cartItem__name">{singleProduct.name}</div>
             <div className="cartItem__amount centered-row">
-                <button className="minusBtn opacity" type="button">
+                <button 
+                    className="minusBtn high-opacity" 
+                    type="button"
+                    onClick={() => decreaseAmount(singleProduct._id)}
+                    disabled={productItems.length === 1}
+                >
                     <MinusIcon />
                 </button>
-                <div className="cartItem__amount_number">10</div>
-                <button className="plusBtn opacity" type="button">
+                <div className="cartItem__amount_number">{productItems.length}</div>
+                <button 
+                    className="plusBtn high-opacity" 
+                    type="button" 
+                    onClick={() => increaseAmount(singleProduct._id)}
+                    disabled={singleProduct.amount === productItems.length}
+                >
                     <PlusIcon />
                 </button>
             </div>
             <div className="cartItem__price">
-                ₴600
+                ₴{singleProduct.price * productItems.length}
             </div>
-            <button className="cartItem__delete high-opacity"><CloseIcon /></button>
+            <button className="cartItem__delete high-opacity" onClick={() => deleteProduct(singleProduct._id)}><CloseIcon /></button>
         </div>
     );
 }
