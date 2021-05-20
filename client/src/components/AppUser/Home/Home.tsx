46b19206460b50b669e55c6e9df94b5b3154b0ba
unknown <@finik848848@gmail.com>
2021-05-20 21:38:54 +0300

    dividing all functionality into few components

--- client/src/components/AppUser/Home/Home.tsx
+++ client/src/components/AppUser/Home/Home.tsx
@@ -3,41 +3,38 @@ import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import img from './../../../assets/slider.jpg';
+import defaultSliderImg from './../../../assets/slider.jpg';
 import PrevButton from './Arrows/PrevButton';
 import NextButton from './Arrows/NextButton';
+import { useSelector } from 'react-redux';
+import { StateType } from '../../../types/stateTypes';
+import HomeDiscounsSlider from './HomeDiscounsSlider';
+import HomeLatestProducts from './HomeLatestProducts';
 
 export type SliderButtonsType = {
     onClick?: () => void;
 }
 
+const sliderSettings = {
+    dots: false,
+    infinite: true,
+    draggable: false,
+    adaptiveHeight: true,
+    autoplay: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: <PrevButton />,
+    nextArrow: <NextButton />
+}
+
 const Home: FC = () => {
-    const settings = {
-        dots: false,
-        infinite: true,
-        draggable: false,
-        adaptiveHeight: true,
-        autoplay: true,
-        speed: 1000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow: <PrevButton />,
-        nextArrow: <NextButton />
-    }
+    const {discounts} = useSelector((state: StateType) => state.discount);
 
     return (
-        <div className="slider">
-            <Slider {...settings} >
-                <div className="slider__item">
-                    <img src={img} alt="" />
-                </div>
-                <div className="slider__item">
-                    <img src={img} alt="" />
-                </div>
-                <div className="slider__item">
-                    <img src={img} alt="s" />
-                </div>
-            </Slider>
+        <div className="homePage">
+            <HomeDiscounsSlider discounts={discounts} />
+            <HomeLatestProducts />
         </div>
     )
 }
