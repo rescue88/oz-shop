d8faf4bef0cbee3b1e9d143307f7d69e0d4e4f4e
unknown <@finik848848@gmail.com>
2021-05-23 14:28:45 +0300

    drawing and styling change orders page

--- client/src/components/AppAdmin/ChangeOrders/ChangeOrders.tsx
+++ client/src/components/AppAdmin/ChangeOrders/ChangeOrders.tsx
@@ -1,8 +1,48 @@
-import { FC } from 'react';
+import { FC, useState, useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getOrders } from '../../../redux/reducers/adminReducer';
+import { StateType } from '../../../types/stateTypes';
+import ChangeOrdersLoader from '../../common/Loader/ChangeOrdersLoader';
+import ChangeOrdersItem from './ChangeOrdersItem/ChangeOrdersItem';
 
 const ChangeOrders: FC = () => {
+    const [isFetching, setIsFetching] = useState<boolean>(false);
+    const dispatch = useDispatch();
+    const orders = useSelector((state: StateType) => state.admin.changeOrders);
+    // console.log(orders);
+
+    const getOrdersHandler = useCallback(async () => {
+        setIsFetching(true);
+
+        await dispatch(getOrders());
+
+        setIsFetching(false);
+    }, []);
+
+    useEffect(() => {
+        getOrdersHandler();
+    }, [getOrdersHandler]);
+
     return (
-        <div>ChangeOrders Page</div>
+        <div className="changeContainer">
+            <div className="changeContainer__header">Редагування замовлень</div>
+            <hr />
+            <div className="changeContainer__content changeBlock">
+                <div className="changeBlock__items changeOrders">
+                    {
+                        isFetching ? (
+                            Array(3).fill(0).map((item, idx) => <ChangeOrdersLoader key={idx} />)
+                        ) : orders.length ? (
+                            orders.map(item => (
+                                <ChangeOrdersItem key={item._id} order={item} isFetching={isFetching} />
+                            ))
+                        ) : (
+                            <div className="no-items">Список замовлень порожній</div>
+                        )
+                    }
+                </div>
+            </div>
+        </div>
     )
 }
 
