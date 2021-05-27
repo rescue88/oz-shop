508edb62095719dd361120364357b6391c267a6e
rescue88 <finik848848@gmail.com>
2021-05-27 14:20:58 +0300

    drawing discount items

--- client/src/components/AppUser/DiscountPage/DiscountPage.tsx
+++ client/src/components/AppUser/DiscountPage/DiscountPage.tsx
@@ -1,7 +1,27 @@
 import { FC } from 'react';
+import { useSelector } from 'react-redux';
+import { StateType } from '../../../types/stateTypes';
+import DiscountPageItem from './DiscountPageItem';
 
 const DiscountPage: FC = () => {
-    return <div>Discount page</div>
+    const {discounts} = useSelector((state: StateType) => state.discount);
+
+    return (
+        <div className="discounts">
+            <div className="discounts__header">Наявні знижки</div>
+            <div className="discounts__items">
+                {
+                    discounts.length ? (
+                        discounts.map(item => (
+                            <DiscountPageItem discount={item} />
+                        ))
+                    ) : (
+                        <div className="no-items">Наразі немає діючих знижок</div>
+                    )
+                }
+            </div>
+        </div>
+    );
 }
 
 export default DiscountPage;
